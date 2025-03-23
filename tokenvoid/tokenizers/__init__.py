404d30e41d28c145304e061de0b4d69cf0471ad8
Rodalit <ryzyevtimur@gmail.com>
2025-03-23 21:17:05 +0300

    Update __init__.py

--- tokenvoid/tokenizers/__init__.py
+++ tokenvoid/tokenizers/__init__.py
@@ -1,3 +1,4 @@
 from .char_tokenizer import CharacterTokenizer
 from .word_tokenizer import WordTokenizer
 from .bpe_tokenizer import BPETokenizer
+from .sentences_tokenizer import SentencesTokenizer
