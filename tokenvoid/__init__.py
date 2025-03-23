e20c8da8da40d3fef1515595914c26316e4d5828
Rodalit <ryzyevtimur@gmail.com>
2025-03-23 20:31:15 +0300

    Update __init__.py

--- tokenvoid/__init__.py
+++ tokenvoid/__init__.py
@@ -1,7 +1,9 @@
 from .tokenizers.char_tokenizer import CharacterTokenizer
 from .tokenizers.word_tokenizer import WordTokenizer
+from .tokenizers.bpe_tokenizer import BPETokenizer
 
 __all__ = [
 	"CharacterTokenizer",
-	"WordTokenizer"
+	"WordTokenizer",
+	"BPETokenizer"
 ]
