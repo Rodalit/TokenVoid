b4ca0c43d33065849e9e43b094205152a77397ca
Rodalit <ryzyevtimur@gmail.com>
2025-03-24 20:51:26 +0300

    Refactored char_tokenizer.py

--- tokenvoid/tokenizers/char_tokenizer.py
+++ tokenvoid/tokenizers/char_tokenizer.py
@@ -1,10 +1,7 @@
 import json
+from .base_tokenizer import BaseTokenizer
 
-class CharacterTokenizer:
-    def __init__(self):
-        self.char2idx = {}
-        self.idx2char = {}
-
+class CharacterTokenizer(BaseTokenizer):
     def train(self, text: str) -> None:
         """
         Trains the tokenizer on the provided text.
@@ -12,58 +9,13 @@ class CharacterTokenizer:
         Args:
             text (str): Text for training
         """
+        self._init_special_tokens()
 
         chars = sorted(set(text))
 
-        self.char2idx = {c: i for i, c in enumerate(chars)}
-        self.idx2char = {i: c for i, c in enumerate(chars)}
+        for idx, token in enumerate(chars, start=len(self.vocab)):
+            self.vocab[token] = idx
+            self.inv_vocab[idx] = token
 
     def tokenize(self, text: str) -> list:
         return list(text)
-
-    def encode(self, text: str) -> list:
-        """
-        Sequence Encoding Function.
-
-        Args:
-            text (str): Text to be encoded
-        """
-        if not text:
-            raise ValueError("The field cannot be empty")
-
-        return [self.char2idx[char] for char in list(text)]
-
-    def decode(self, tokens: list) -> str:
-        """
-        Decoding a sequence of tokens back to text
-
-        Args:
-            tokens (list): Token sequence
-        """
-        if not tokens:
-            raise ValueError("The field cannot be empty")
-
-        return "".join(self.idx2char[index] for index in tokens)
-
-    def save(self, filepath: str) -> None:
-        """
-        Function for saving token dictionaries
-
-        Args:
-            filepath (str): The path where you need to save the file, do not forget to specify the file name and its format (json)
-        """
-
-        with open(filepath, "w", encoding="utf-8") as file:
-            json.dump(self.char2idx, file)
-
-    def load(self, filepath: str) -> None:
-        """
-        Function for loading a token dictionary
-
-        Args:
-            filepath (str): Path to the file with dictionaries
-        """
-
-        with open(filepath, "r", encoding='utf-8') as file:
-            self.char2idx = json.load(file)
-            self.idx2char = {i: c for c, i in self.char2idx.items()}
