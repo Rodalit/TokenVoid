9ab891e85e4cf6f44e17d5038284be46bf42f984
Rodalit <ryzyevtimur@gmail.com>
2025-04-01 18:45:16 +0300

    Delete tokenvoid directory

--- tokenvoid/tokenizers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .char_tokenizer import CharacterTokenizer
-from .word_tokenizer import WordTokenizer
-from .bpe_tokenizer import BPETokenizer
-from .sentences_tokenizer import SentencesTokenizer
