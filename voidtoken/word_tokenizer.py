f7cea0e3d69de393c8b2aa3712c10ec06845ba17
Rodalit <ryzyevtimur@gmail.com>
2025-04-02 18:29:11 +0300

    Add files via upload

--- voidtoken/word_tokenizer.py
+++ voidtoken/word_tokenizer.py
@@ -4,20 +4,23 @@ from collections import Counter
 from .base_tokenizer import BaseTokenizer
 
 class WordTokenizer(BaseTokenizer):
-    def train(self, text: str) -> None:
+    def train(self, corpus: str) -> None:
         """
         Train the tokenizer on the corpus
 
         Args:
-            text (str): Text on which the tokenizer will be trained
+            corpus (list): Training data set.
         """
         self._init_special_tokens()
 
-        tokens = self.tokenize(text)
+        tokens = []
+
+        for text in corpus:
+            tokens.extend(self.tokenize(text))
 
         freq = Counter(tokens)
 
-        most_popular_words = [word for word, _ in freq.most_common(self.vocab_size)]
+        most_popular_words = [word for word, _ in freq.most_common(self.vocab_size - len(self.vocab))]
 
         for index, token in enumerate(most_popular_words, start=len(self.vocab)):
             self.vocab[token] = index
