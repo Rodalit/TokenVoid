eb0a783338e3bfd77d9de031d9ed7557dde8a522
Rodalit <ryzyevtimur@gmail.com>
2025-04-07 18:12:39 +0300

    Changes have been made to encoding and decoding

--- voidtoken/base_tokenizer.py
+++ voidtoken/base_tokenizer.py
@@ -5,13 +5,19 @@ from collections import Counter
 class BaseTokenizer:
     def __init__(self, 
         vocab_size: int = 10_000, 
-        special_tokens: dict = {"pad_token": "<|pad|>", "unk_token": "<|unk|>",
-                                "bos_token": "<|bos|>", "eos_token": "<|eos|>"}):
+        special_tokens: dict = {"unk_token": "[UNK]", 
+                                "bos_token": "[BOS]", 
+                                "eos_token": "[EOS]"
+                                }):
         self.vocab_size = vocab_size
         self.special_tokens = special_tokens
         self.unk_token = self.special_tokens["unk_token"]
+        self.bos_token = self.special_tokens["bos_token"]
+        self.eos_token = self.special_tokens["eos_token"]
         self.vocab = {}
         self.inv_vocab = {}
+
+        self.add_special_pattern = None
 
     def _init_special_tokens(self) -> None:
         for token in self.special_tokens:
@@ -32,14 +38,23 @@ class BaseTokenizer:
 
     def encode(self, text: str, add_special_tokens: bool = False, classification: bool = False):
         if add_special_tokens:
-            if "bos_token" in self.special_tokens and "eos_token" in self.special_tokens:
-                tokens = [self.special_tokens["bos_token"]] + self.tokenize(text) + [self.special_tokens["eos_token"]]
-                return [self.vocab.get(token, self.vocab[self.unk_token]) for token in tokens]
+            tokens = [self.bos_token] + self.tokenize(text) + [self.eos_token]
+            return [self.vocab.get(token, self.vocab[self.unk_token]) for token in tokens]
 
         return [self.vocab.get(token, self.vocab[self.unk_token]) for token in self.tokenize(text)]
 
-    def decode(self, tokens: list) -> str:
-        return "".join(self.inv_vocab[token] for token in tokens)
+    def decode(self, tokens: list, remove_special_tokens: bool = False) -> str:
+        if remove_special_tokens:
+            
+            text = "".join(self.inv_vocab.get(token, self.unk_token) for token in tokens)
+            
+            if not self.add_special_pattern:
+                # Caching the compiled regular expression
+                self.special_pattern = re.compile("|".join(map(re.escape, self.special_tokens.values())))
+            
+            return self.special_pattern.sub("", text)
+        
+        return "".join(self.inv_vocab.get(token, self.unk_token) for token in tokens)
 
     def save(self, filepath: str) -> None:
         with open(filepath, "w", encoding="utf-8") as file:
